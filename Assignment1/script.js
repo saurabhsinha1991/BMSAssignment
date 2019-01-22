7f5ec0ee807cad5c52ac11f1b57d4f3fe6c53e19
ssi253 <ssinha139@sapient.com>
2019-01-23 01:39:41 +0530

    fix

--- Assignment1/script.js
+++ Assignment1/script.js
@@ -11,26 +11,31 @@
 
     let app = {
         list: {},
+        prevValue: '',
         attachEvent() {
             let input =  document.getElementById('input'),
                 that = this;
             
             input.addEventListener('keyup', (e) => debounce(function() {
-                if (!that.list[e.target.value]) {
-                    if (e.target.value.indexOf('-') > -1) {
-                        let splitArray = e.target.value.split('-');
-                        if (!that.list[splitArray[0]] && !that.list[splitArray[1]]) {
-                            for (let i = splitArray[0]; i <= splitArray[1]; i++) {
+                if (e.target.value.length > 0 && that.prevValue !== e.target.value && !that.list[e.target.value]) {
+                    that.prevValue = e.target.value;
+
+                    let digits = e.target.value.split(',');
+
+                    digits.forEach(eachDigit => {
+                        if (eachDigit.indexOf('-') > -1) {
+                            let splitArray = eachDigit.split('-');
+                            for (let i = parseInt(splitArray[0]); i <= parseInt(splitArray[1]); i++) {
                                 that.list[i] = ( that.list[i] || 0 ) + 1;
                             }
                         }
-                    }
-                    else {
-                        that.list[e.target.value] = ( that.list[e.target.value] || 0 ) + 1;
-                    }
+                        else {
+                            that.list[parseInt(eachDigit)] = ( that.list[parseInt(eachDigit)] || 0 ) + 1;
+                        }
+                    });
                     that.sagregatre();
                 }
-            }, 2000)());
+            }, 5000)());
         },
         sagregatre() {
             let uniqueElement = [], duplicateELement = [];
